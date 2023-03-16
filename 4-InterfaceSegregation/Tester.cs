4aac0644d6ae34c397c732ab05d4fa4f0a7443db
Victor Olvera <volvera.thome@gmail.com>
2023-03-15 21:38:40 -0600

    liskov and Interface Segregation

--- 4-InterfaceSegregation/Tester.cs
+++ 4-InterfaceSegregation/Tester.cs
@@ -1,29 +1,9 @@
 namespace InterfaceSegregation
 {
-    public class Tester : IActivities
+    public class Tester : ITestActivities
     {
         public Tester()
         {
-        }
-
-        public void Plan() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Comunicate() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Design() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Develop() 
-        {
-            Console.WriteLine("I'm developing the functionalities required");
         }
 
         public void Test() 
